32377b00d462b6ab444eecf4eda96b38163b2d72
Glenn Bishop Jr <sifelaver@gmail.com>
2018-01-14 20:47:29 -0500

    Fix namespace issue

--- FemaAPIWrapper/FemaInfo/DisasterDeclarationsSummaries.cs
+++ FemaAPIWrapper/FemaInfo/DisasterDeclarationsSummaries.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FemaAPIWrapper.DisasterDeclarationsSummaries
+namespace FemaAPIWrapper.FemaInfo
 {
     public class DisasterDeclarationsSummaries
     {
